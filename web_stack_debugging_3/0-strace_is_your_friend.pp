0ea1a934b0e0150a9ca394f97af0c8ffbb324039
TRolande <r.tumugane@alustudent.com>
2025-08-04 12:07:53 -0700

    my files

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,6 @@
- puppet manuscript to replace a line in a file on a server
+$ apt-get install -y ruby
+$ gem install puppet-lint -v 2.1.1
+# A puppet manuscript to replace a line in a file on a server
 
 $file_to_edit = '/var/www/html/wp-settings.php'
 
