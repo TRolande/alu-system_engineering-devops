bfc9ce5f9e2abd251d48199d6a132b3cc0f14930
TRolande <r.tumugane@alustudent.com>
2025-08-04 12:48:16 -0700

    project

--- web_stack_debugging_4/1-user_limit.pp
+++ web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,5 @@
-# This Puppet manifest increases the nofile limit for the holberton user to prevent "Too many open files" errors
+# This Puppet manifest raises the file descriptor limit for the holberton user
+# to fix "Too many open files" errors on login and when opening files
 
 file_line { 'pam_limits_session':
   ensure => present,
@@ -17,14 +18,14 @@ file_line { 'pam_limits_noninteractive':
 file_line { 'holberton_soft_nofile':
   ensure => present,
   path   => '/etc/security/limits.conf',
-  line   => 'holberton soft nofile 1024',
+  line   => 'holberton soft nofile 4096',
   match  => '^holberton\s+soft\s+nofile',
 }
 
 file_line { 'holberton_hard_nofile':
   ensure => present,
   path   => '/etc/security/limits.conf',
-  line   => 'holberton hard nofile 4096',
+  line   => 'holberton hard nofile 65535',
   match  => '^holberton\s+hard\s+nofile',
 }
 
